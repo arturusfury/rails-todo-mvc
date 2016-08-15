ad25447f00b2718c6395ac847368f8ab0198f0a1
arturusfury <bri.reynolds@gmail.com>
2016-08-15 10:15:28 -0500

    added code for changing the status of an item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,28 @@
+# Items Controller
 class ItemsController < ApplicationController
   def create
     @list = List.find(params[:list_id])
     @item = @list.items.build(item_params)
 
     if @item.save
+      redirect_to @list
+    else
+      @items = List.find(params[:list_id]).items
+      render 'lists/show'
+    end
+  end
+
+  def update
+    @list = List.find(params[:list_id])
+    @item = @list.items.find(params[:id])
+
+    if @item
+      if @item.complete?
+        @item.incomplete!
+      else
+        @item.complete!
+      end
+
       redirect_to @list
     else
       @items = List.find(params[:list_id]).items
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,21 @@
+# Item Class
 class Item < ApplicationRecord
   belongs_to :list
 
   validates :name, presence: true
+
+  def complete?
+    return true if completed
+    false
+  end
+
+  def complete!
+    self.completed = true
+    save
+  end
+
+  def incomplete!
+    self.completed = false
+    save
+  end
 end
